ca0a4a4524dee532255a1c69d7e004cace91a260
ASUKA_RISFIN <116791022+kieutanphuoc84tv@users.noreply.github.com>
2023-11-07 14:51:16 +0700

    Update  Ct_DAOTAO

--- QLSV.sql
+++ QLSV.sql
@@ -1,5 +1,5 @@
-Create Database QLSV
-Use QLSV
+Create Database CT_DAOTAO
+Use CT_DAOTAO
 
 
 Create table CT_Daotao
@@ -87,6 +87,60 @@ SP_pkeys SinhVien
 SP_PKEYS KetQuaThi
 
 
+Insert INTO CT_DaoTao VALUES ('1',N'CỬ NHÂN')
+Insert INTO CT_Daotao VALUES ('2','Cao Đẳng')
+Insert INTO CT_Daotao VALUES ('3','Trung Cấp')
 
+SELECT * 
+FROM Khoahoc
+
+Insert INTO Khoahoc VALUES ('1','1','Khóa 1 năm 2005')
+Insert INTO Khoahoc Values ('2','1','Khóa 2 năm 2006')
+Insert INTO Khoahoc values ('3','2','Khóa 1 năm 2007')
+insert INTO Khoahoc values ('4','3','Khóa 1 năm 2008')
+insert INTO Khoahoc values ('5','2','Khóa 2 năm 2008')
+
+
+select*
+from Lop
+insert INTO Lop values ('1','1','K1_1')
+insert INTO Lop values	('2','1','K1_1')
+insert INTO Lop values	('3','1','K1_1')
+insert INTO Lop values	('4','2','K1_2')
+select*
+from MonHoc
+
+Insert INTO   MonHoc VALUES ('TH101','Tin học đại cương')
+Insert INTO  MonHoc VALUES ('TH110','Toán rời rạc')
+Insert INTO  MonHoc VALUES ('TH102','Cơ sở dữ liệu')
+
+SET DATEFORMAT DAY
+
+SELECT * 
+FROM CT_DAOTAO
+SET DATEFORMAT DMY 
+Insert INTO  SinhVien VALUES ('K116','Đinh Nguyễn Vân Anh',N'Nữ','18/12/1985','Bình Thuận','1')
+Insert INTO  SinhVien VALUES ('K117','Trần Hoàng Anh','Nữ','4/8/1986','Bình Dương','1')
+Insert INTO  SinhVien VALUES ('K118','Bùi Thanh Bình','Nam','23/10/1985','Đồng Nai','1')
+Insert INTO  SinhVien VALUES ('K119','Phạm Nhật Tân','Nam','15/1/1985','Bình Tháp','2')
+Insert INTO  SinhVien VALUES ('K120','Định Thiện Hòa','Nam','8/12/1985','Vĩnh Long','3')
+Insert INTO  SinhVien VALUES ('K121','Nguyễn Anh Khoa','Nam','14/11/1986','Vũng Tàu','3')
+Insert INTO  SinhVien VALUES ('K122','Trương Mộng Thúy','Nữ','12/5/1986','Cần Thơ','2')
+Insert INTO  SinhVien VALUES ('K123','Võ Anh Thi','Nữ','23/5/1986','Bến Tre','2')
+
+Select *
+from KetQuaThi
+
+Insert INTO  KetQuaThi VALUES (' K116','TH101','1','8')
+Insert INTO  KetQuaThi VALUES (' K117','TH101','1','4')
+Insert INTO  KetQuaThi VALUES (' K118','TH101','1','6.5')
+Insert INTO  KetQuaThi VALUES (' K119','TH102','1','5')
+Insert INTO  KetQuaThi VALUES (' K120','TH102','1','7')
+Insert INTO  KetQuaThi VALUES (' K121','TH110','1','7')
+Insert INTO  KetQuaThi VALUES (' K122','TH102','1','9')
+Insert INTO  KetQuaThi VALUES (' K119','TH101','1','6')
+Insert INTO  KetQuaThi VALUES (' K120','TH101','1','5')
+Insert INTO  KetQuaThi VALUES (' K102','TH102','1','7.5')
+Insert INTO  KetQuaThi VALUES (' K11','TH101','1','7')
 
 
